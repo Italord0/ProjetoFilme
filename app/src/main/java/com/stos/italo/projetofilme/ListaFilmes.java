3ade03537a564df998955a4c2f12b3a52d29e30d
Italo <italord.melo@gmail.com>
2018-09-06 20:46:44 -0300

    primeiro commit

---
+++ app/src/main/java/com/stos/italo/projetofilme/ListaFilmes.java
@@ -0,0 +1,13 @@
+package com.stos.italo.projetofilme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaFilmes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_filmes);
+    }
+}
